29b0454e047be40cf18b1c1958a5dd2aae29ba53
Sunitha Kolli <sunitha@olivetech.net>
2018-04-18 18:28:55 +0530

    Added the components and navigation to the links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { MatCardModule, MatIconModule, MatToolbarModule, MatButtonModule, MatFor
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatMenuModule, MatMenuItem } from '@angular/material/menu';
 import {MatDividerModule} from '@angular/material/divider';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -24,10 +36,13 @@ import { AppComponent } from './app.component';
     MatInputModule,
     FlexLayoutModule,
     MatMenuModule,
-    MatDividerModule   
+    MatDividerModule,
+    RouterModule.forRoot(routes)
 
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
